2fba6997168d48bc3ec58cf9074834519bc3f4ba
jfernandesdev <jfernandes.dev@gmail.com>
2021-09-10 10:12:21 -0300

    authentication context creation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useHistory } from 'react-router-dom';
+import { useContext } from 'react';
 
-import { firebase, auth } from '../services/firebase';
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -12,14 +13,13 @@ import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-      history.push('/rooms/new');
-    })
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push('/rooms/new');
   }
 
   return (
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { AuthContext } from '../App';
+
 import { Link } from 'react-router-dom'
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -8,6 +11,7 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function NewRoom() {
+  const { user } = useContext(AuthContext);
   return (
     <div id="page-auth">
       <aside>
@@ -18,6 +22,7 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Logo Letmeask" />
+          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
             <input
